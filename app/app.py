6fc572209577f3a05b6fb3aa98bb09a66fa495d7
harsh1214 <harsh.yadav.1412@gmail.com>
2026-02-26 23:04:18 +0530

    Updated 03

--- app/app.py
+++ app/app.py
@@ -4,11 +4,9 @@ from fastapi.responses import FileResponse
 from pathlib import Path
 from pydantic import BaseModel
 from app.predict import predict
-from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_PATH = BASE_DIR / "static"
+STATIC_PATH = BASE_DIR / "static" / "index.html"
 
 app = FastAPI()
 
@@ -17,20 +15,9 @@ class Request(BaseModel):
     sentence: str
     aspect: str
 
-tokenizer = None
-model = None
-MODEL_NAME = "harsh1214/feedback-analyzer"
-
-@app.on_event("startup")
-async def startup_event():
-    global tokenizer, model
-    tokenizer = DistilBertTokenizerFast.from_pretrained(MODEL_NAME)
-    model = DistilBertForSequenceClassification.from_pretrained(MODEL_NAME)
-    model.eval()
-
 @app.get('/')
-def root():
-    return FileResponse(STATIC_PATH / "index.html")
+def serve_index():
+    return FileResponse(STATIC_PATH)
 
 @app.post('/api/predict/')
 def predict_feedback(req: Request):
