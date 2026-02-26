4a0929eb4f9881c9f401da5d7f5467caf3559264
harsh1214 <harsh.yadav.1412@gmail.com>
2026-02-26 22:16:51 +0530

    Test 01

--- app/app.py
+++ app/app.py
@@ -1,24 +1,25 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from pathlib import Path
-from pydantic import BaseModel
-from app.predict import predict
+# from pathlib import Path
+# from pydantic import BaseModel
+# from app.predict import predict
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_PATH = BASE_DIR / "static"
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# STATIC_PATH = BASE_DIR / "static"
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=str(STATIC_PATH)), name="static")
-class Request(BaseModel):
-    sentence: str
-    aspect: str
+# app.mount("/static", StaticFiles(directory=str(STATIC_PATH)), name="static")
+# class Request(BaseModel):
+#     sentence: str
+#     aspect: str
 
 @app.get('/')
 def root():
-    return FileResponse(STATIC_PATH / "index.html")
+    return {"message": "Hello World"}
+    # return FileResponse(STATIC_PATH / "index.html")
 
-@app.post('/api/predict/')
-def predict_feedback(req: Request):
-    return predict(req.sentence, req.aspect)
+# @app.post('/api/predict/')
+# def predict_feedback(req: Request):
+#     return predict(req.sentence, req.aspect)
