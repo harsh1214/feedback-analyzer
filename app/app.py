1310f32cd8eae9c9ceedda0ab4a49ef3af64a1e5
harsh1214 <harsh.yadav.1412@gmail.com>
2026-02-28 16:37:51 +0530

    Test 09

--- app/app.py
+++ app/app.py
@@ -2,21 +2,21 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pathlib import Path
 from pydantic import BaseModel
-# from app.predict import predict
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 INDEX_FILE = BASE_DIR / "static" / "index.html"
 
 app = FastAPI()
 
-# class Request(BaseModel):
-#     sentence: str
-#     aspect: str
+class Request(BaseModel):
+    sentence: str
+    aspect: str
 
 @app.get('/')
 def serve_index():
     return FileResponse(INDEX_FILE)
 
-# @app.post('/api/predict/')
-# def predict_feedback(req: Request):
-#     return predict(req.sentence, req.aspect)
+@app.post('/api/predict/')
+def predict_feedback(req: Request):
+    from app.predict import predict
+    return predict(req.sentence, req.aspect)
